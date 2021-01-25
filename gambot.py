b96f89185cfd94f82500113ae11bc1cad2643b85
dlarocque <daniellrq@outlook.com>
2021-01-25 15:18:52 -0600

    Converted from client to bot

--- gambot.py
+++ gambot.py
@@ -3,30 +3,29 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
     print('Gambot is connected to Discord.')
     print('Connected to the following guilds :')
 
-    for guild in client.guilds:
+    for guild in bot.guilds:
         print(guild.name)
 
-@client.event
+@bot.event
 async def on_message(message):
 
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content == 'Hey Gambot':
         await message.channel.send(f'Hey {message.author.display_name} :)')
-    if message.content == '!github':
-        await message.channel.send(f'https://github.com/dlarocque/Gambot')
 
-client.run(TOKEN)
+bot.run(TOKEN)
