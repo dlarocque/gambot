1046ff98f0ed9b886ac2c510601db6503cbc4281
dlarocque <daniellrq@outlook.com>
2021-01-25 12:28:11 -0600

    Included list of connected guilds on startup

--- gambot.py
+++ gambot.py
@@ -6,11 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Gambot is connected to Discord.')
+    print('Connected to the following guilds :')
+
+    for guild in client.guilds:
+        print(guild.name)
+
 
 client.run(TOKEN)
