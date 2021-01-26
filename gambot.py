b846ecb2bcef400b8e96abd29747f0b0606bcc05
dlarocque <daniellrq@outlook.com>
2021-01-26 12:43:44 -0600

    Listed deathroll commands

--- gambot.py
+++ gambot.py
@@ -28,11 +28,25 @@ async def on_message(message):
     if message.content == 'Hey Gambot':
         await message.channel.send(f'Hey {message.author.display_name} :)')
 
+    # The on_message method looks at every single message, and can prevent
+    # bot commands from being read in, so we do this to prevent that.
     await bot.process_commands(message)
 
-@bot.command(name='github')
+@bot.command(name='github', help='Sends a link to Gambot\'s GitHub repo.')
 async def github(ctx):
     print('user asked for github')
     await ctx.send('https://github.com/dlarocque/Gambot')
 
+@bot.command(name='points')
+async def points(ctx):
+    # todo
+
+@bot.command(name='deathroll')
+async def deathroll(ctx, bet, opponent):
+    await ctx.send(f'{ctx.author.display_name} has started a deathroll with @{opponent.display_name}')
+    # todo
+
+@bot.command(name='roll')
+async def roll(ctx, bet):
+    # todo
 bot.run(TOKEN)
